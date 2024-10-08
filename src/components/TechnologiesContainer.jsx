b628e191c60814866d02b1d3bd76dfb523546de2
Hiag0liveira <hiagophd@gmail.com>
2024-10-08 13:37:36 -0300

    refactor: Add new technologies to TechnologiesContainer
    - Added icons and names for new technologies: Docker, Git, Java, MongoDB, NestJS, Next.js, PostgreSQL, Prisma ORM, Python, Spring Boot, Spring Security, Tailwind CSS, Thymeleaf, TypeScript, and Flutter.
    - Updated the order of technologies to maintain consistency.

--- src/components/TechnologiesContainer.jsx
+++ src/components/TechnologiesContainer.jsx
@@ -1,21 +1,62 @@
 import {
-  DiHtml5,
+  DiBootstrap,
   DiCss3,
+  DiDocker,
+  DiGit,
+  DiHtml5,
+  DiJava,
   DiJsBadge,
-  DiNodejsSmall,
   DiMysql,
+  DiNodejsSmall,
+  DiPostgresql,
+  DiPython,
   DiReact,
+  DiSass,
 } from "react-icons/di";
+
+import {
+  SiExpress,
+  SiFlutter,
+  SiMongodb,
+  SiNestjs,
+  SiSpringboot,
+  SiSpringsecurity,
+  SiThymeleaf,
+  SiTypescript,
+  SiTailwindcss,
+  SiPrisma,
+  SiMongoose,
+  SiNextdotjs,
+} from "react-icons/si";
 
 import "../styles/components/technologiescontainer.sass";
 
 const technologies = [
   { id: "html", name: "HTML5", icon: <DiHtml5 /> },
   { id: "css", name: "CSS3", icon: <DiCss3 /> },
-  { id: "js", name: "JavaScript", icon: <DiJsBadge /> },
-  { id: "node", name: "Node.js", icon: <DiNodejsSmall /> },
-  { id: "mysql", name: "MySQL", icon: <DiMysql /> },
   { id: "react", name: "React", icon: <DiReact /> },
+  { id: "nextjs", name: "Next.js", icon: <SiNextdotjs /> },
+  { id: "bootstrap", name: "Bootstrap", icon: <DiBootstrap /> },
+  { id: "sass", name: "Sass", icon: <DiSass /> },
+  { id: "tailwind", name: "Tailwind CSS", icon: <SiTailwindcss /> },
+  { id: "thymeleaf", name: "Thymeleaf", icon: <SiThymeleaf /> },
+  { id: "js", name: "JavaScript", icon: <DiJsBadge /> },
+  { id: "express", name: "Express", icon: <SiExpress /> },
+  { id: "node", name: "Node.js", icon: <DiNodejsSmall /> },
+  { id: "typescript", name: "TypeScript", icon: <SiTypescript /> },
+  { id: "nestjs", name: "NestJS", icon: <SiNestjs /> },
+  { id: "java", name: "Java", icon: <DiJava /> },
+  { id: "springboot", name: "Spring Boot", icon: <SiSpringboot /> },
+  { id: "springsecurity", name: "Spring Security", icon: <SiSpringsecurity /> },
+  { id: "python", name: "Python", icon: <DiPython /> },
+  { id: "flutter", name: "Flutter", icon: <SiFlutter /> },
+  { id: "docker", name: "Docker", icon: <DiDocker /> },
+  { id: "git", name: "Git", icon: <DiGit /> },
+  { id: "mysql", name: "MySQL", icon: <DiMysql /> },
+  { id: "mongodb", name: "MongoDB", icon: <SiMongodb /> },
+  { id: "postgresql", name: "PostgreSQL", icon: <DiPostgresql /> },
+  { id: "prisma", name: "Prisma ORM", icon: <SiPrisma /> },
+  { id: "mongoose", name: "Mongoose ODM", icon: <SiMongoose /> },
 ];
 
 const TechnologiesContainer = () => {
