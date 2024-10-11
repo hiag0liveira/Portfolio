051a8eafaf65bbf21000ae08423938d5b4a2c2c1
Hiag0liveira <hiagophd@gmail.com>
2024-10-11 13:46:30 -0300

    feat: Pagination projects cards

---
+++ src/components/Pagination.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import "../styles/components/pagination.sass";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+import { MdOutlineKeyboardArrowLeft } from "react-icons/md";
+
+const Pagination = ({ currentPage, totalPages, onPageChange }) => {
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      onPageChange(currentPage - 1);
+    }
+  };
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      onPageChange(currentPage + 1);
+    }
+  };
+
+  return (
+    <div className="pagination">
+      <span
+        className="arrow"
+        onClick={handlePrevPage}
+        disabled={currentPage === 1}
+      >
+        <MdOutlineKeyboardArrowLeft size={35} />
+      </span>
+      <span>
+        Página {currentPage} de {totalPages}
+      </span>
+      <span
+        className="arrow"
+        onClick={handleNextPage}
+        disabled={currentPage === totalPages}
+      >
+        <MdOutlineKeyboardArrowRight size={35} />
+      </span>
+    </div>
+  );
+};
+
+export default Pagination;
