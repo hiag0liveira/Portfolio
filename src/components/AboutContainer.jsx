bd974a2188cd3b6e60a3c1298554c20ba88cd40f
Hiago Mendes <hiagophd@gmail.com>
2025-02-13 10:47:17 -0300

    refactor: update the about me description

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -3,15 +3,38 @@ const AboutContainer = () => {
     <section className="about-container">
       <h2>Sobre Mim</h2>
       <p>
-        Desenvolvedor com ampla experiência em tecnologias modernas e
-        habilidades técnicas versáteis. Possuo sólidos conhecimentos em
-        programação orientada a objetos, estruturas de dados e algoritmos, além
-        de experiência com diversas linguagens e frameworks para desenvolvimento
-        web e de software.
+        Sou desenvolvedor full stack e atualmente curso Sistemas de Informação
+        no Instituto Federal Fluminense (IFF), estando no sétimo período, com
+        previsão de conclusão em janeiro de 2026.
       </p>
       <p>
-        Atualmente, atuo como desenvolvedor em um projeto do IFF em colaboração
-        com a EMBRAPII, focado no desenvolvimento com NestJS e Flutter.
+        Atuo na LRGE Systems, onde participo do desenvolvimento e manutenção de
+        soluções tecnológicas para diversos clientes. Tenho experiência no
+        desenvolvimento de sistemas web e móveis, trabalhando com tecnologias
+        como PHP com Laravel, Java, C#, React Native, React e NestJS. Minhas
+        principais responsabilidades incluem a implementação de novas
+        funcionalidades, integração de APIs, otimização de desempenho e melhoria
+        contínua das aplicações. Trabalho em estreita colaboração com equipes
+        multifuncionais, garantindo soluções eficientes e alinhadas às
+        necessidades dos clientes.
+      </p>
+      <p>
+        A LRGE Systems é uma empresa especializada no desenvolvimento,
+        customização e aprimoramento de sistemas e ferramentas digitais,
+        abrangendo sistemas embarcados, soluções web e distribuídas, atendendo
+        grandes e médias empresas.
+      </p>
+      <p>
+        Além da experiência profissional, estou envolvido em um estágio parcial
+        na faculdade, onde atuo no desenvolvimento do back-end de uma aplicação
+        utilizando NestJS.
+      </p>
+      <p>
+        Possuo sólidos conhecimentos em Domain-Driven Design (DDD), estrutura de
+        dados e algoritmos, além de experiência prática no desenvolvimento de
+        aplicações escaláveis e performáticas. Gosto de desafios técnicos, de
+        aprender novas tecnologias e de contribuir para projetos inovadores que
+        impactam positivamente o ambiente corporativo e os usuários finais.
       </p>
     </section>
   );
